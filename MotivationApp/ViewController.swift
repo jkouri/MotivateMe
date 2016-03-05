4c3009ed8497d282422b20e64dd0f7711ed68753
Jacqueline Kouri <jackie@kourifamily.com>
2016-03-04 21:45:05 -0500

    Initial Commit

---
+++ MotivationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotivationApp
+//
+//  Created by Jacqueline Kouri on 3/4/16.
+//  Copyright © 2016 Jacqueline Kouri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
